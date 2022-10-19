91ed769e6c89f8a5aa754452623b5e46bd5febf1
Gaspar Bonari <gasparbonari@gmail.com>
2022-10-19 16:46:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(window).load(function(){
+	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+
+/******************************************************************************************************************************
+Menu
+*******************************************************************************************************************************/ 
+(function() {
+
+	var bodyEl = document.body,
+		//content = document.querySelector( '.content-wrap' ),
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false;
+
+	function init() {
+		initEvents();
+	}
+
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleMenu );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenu );
+		}	
+	}
+
+	function toggleMenu() {
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+		}
+		isOpen = !isOpen;
+	}
+
+	init();
+
+})();
+
+
+/******************************************************************************************************************************
+Typing animation
+*******************************************************************************************************************************/
+
+let point = document.querySelector(".about-p");
+
+function typing(entries)
+{
+  let [entry] = entries;
+
+  if(!entry.isIntersecting) return;
+
+  document.querySelector("h4").classList.remove("hidden");
+
+}
+
+let typeInteration = new IntersectionObserver(typing,
+{
+  root: null,
+  threshold: 0.2
+})
+
+typeInteration.observe(point)
