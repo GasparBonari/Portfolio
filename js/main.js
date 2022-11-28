1a569e4f5ab87ae6c2046093a40df5a5bd2e819b
Gaspar Bonari <gasparbonari@gmail.com>
2022-11-28 18:11:58 +0100

    .

--- js/main.js
+++ js/main.js
@@ -62,4 +62,73 @@ let typeInteration = new IntersectionObserver(typing,
   threshold: 0.2
 })
 
-typeInteration.observe(point)
+typeInteration.observe(point);
+
+
+/******************************************************************************************************************************
+Slider
+*******************************************************************************************************************************/
+
+let slides = document.querySelectorAll(".slide");
+let btnSlideLeft = document.querySelector(".slider__btn--left");
+let btnSlideRight = document.querySelector(".slider__btn--right");
+
+let currentSlide = 0;
+let maxSlide = slides.length;
+
+function goToSlide(slide)
+{
+	slides.forEach((e, i) => 
+	{
+		e.style.transform = `translateX(${50 * (i - slide)}%)`;
+	})
+}
+
+goToSlide(0);
+
+function slideToRight()
+{
+  if(maxSlide - 1 == currentSlide)
+  {
+    currentSlide = 0; // goes back to the first one
+  }
+  else
+  {
+    currentSlide++;
+  }
+
+  goToSlide(currentSlide);
+}
+
+function slideToLeft()
+{
+  if(currentSlide == 0)
+  {
+    currentSlide = maxSlide - 1; // goes to the last one
+  }
+  else
+  {
+    currentSlide--;
+  }
+
+  goToSlide(currentSlide);
+}
+
+btnSlideRight.addEventListener("click", slideToRight);
+btnSlideLeft.addEventListener("click", slideToLeft);
+
+document.addEventListener("keydown", function(e)
+{
+  if(e.key == "ArrowRight")
+  {
+    slideToRight();
+  }
+})
+
+document.addEventListener("keydown", function(e)
+{
+  if(e.key == "ArrowLeft")
+  {
+    slideToLeft();
+  }
+})
